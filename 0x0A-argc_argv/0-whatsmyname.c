f105eff2af4bb45aada4583e242ab96fbd68710f
milkawnoh <milkawinoh@gmail.com>
2023-04-10 17:55:51 -0700

    0x0A. C - argc, argv ptoject

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the program name.
+ * @argc: shows number of arguments supplied to the program.
+ * @argv: An array of pointers to arguments.
+ *
+ * Return:  o.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
