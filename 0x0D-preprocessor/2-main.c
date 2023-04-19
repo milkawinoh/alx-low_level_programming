1f1f31b33767a8244d9b44f2ded021037b70bba5
milkawnoh <milkawinoh@gmail.com>
2023-04-18 19:56:02 -0700

    0x0D. C - Preprocessor project

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file of the program
+ *        compiled from, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
+
