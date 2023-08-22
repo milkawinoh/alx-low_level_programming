b84decd9004ecbe53580ebd810899d278b29a9b2
milkawinoh <milkawinoh@gmail.com>
2023-08-21 19:55:38 -0700

    initial commit

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Sum of a and b
+ */
+int add(int a, int b)
+{
+    return (a + b);
+}
+
+/**
+ * sub - Subtracts two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Difference between a and b
+ */
+int sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+ * mul - Multiplies two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Product of a and b
+ */
+int mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+ * div - Divides two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Result of division of a by b
+ */
+int _div(int a, int b)
+{
+    return (a / b);
+}
+
+/**
+ * mod - Computes the modulus of two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Remainder of the division of a by b
+ */
+int mod(int a, int b)
+{
+    return (a % b);
+}
+
